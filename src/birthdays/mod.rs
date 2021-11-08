fddaabb93db9c8097a17972fabe6795c0c84fef5
maddy-hops <antondal8@gmail.com>
2021-11-08 16:55:27 +0700

    add two global vars,, setup a daily bd message

--- src/birthdays/mod.rs
+++ src/birthdays/mod.rs
@@ -1,10 +1,18 @@
 // module to keep track of user's birthdays through a connection to mongodb db
-use serenity::{client::Context,model::channel::Message,framework::standard::{macros::command, CommandResult}};
-use mongodb::{Client, options::ClientOptions};
-use std::env;
+use serenity::{client::Context, framework::standard::{macros::command, CommandResult}, model::channel::Message, prelude::{RwLock, TypeMapKey}};
+use mongodb::{options::ClientOptions};
+use std::{collections::HashMap, env, sync::Arc};
 use futures::stream::TryStreamExt;
 use mongodb::{bson::doc, options::FindOptions};
 use serde::{Deserialize, Serialize};
+
+
+pub struct BirthdaysDb;
+
+
+impl TypeMapKey for BirthdaysDb {
+    type Value = Arc<RwLock<HashMap<String, u64>>>;
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Birthday {
@@ -17,7 +25,7 @@ struct Birthday {
 pub async fn update_db(_ctx: &Context, msg: &Message) -> CommandResult{
 	let connection_string = env::var("db_connection_string").expect("Database connection string not found");
 	let client_options = ClientOptions::parse(connection_string).await?;
-	let client = Client::with_options(client_options)?;
+	let client = mongodb::Client::with_options(client_options)?;
 	let db = client.database("discord-bot");
 	let birthdays = db.collection::<Birthday>("birthdays");
 	let filter = doc! { "dob": "16/11" };
@@ -27,4 +35,17 @@ pub async fn update_db(_ctx: &Context, msg: &Message) -> CommandResult{
 		println!("{:?}",birthday);
 	}
 	Ok(())
+}
+
+pub async fn notify_users(ctx: &Context, msg: &Message) {
+	let channel = match msg.channel_id.to_channel(&ctx).await {
+		Ok(channel) => channel,
+		Err(why) => {
+			println!("Error getting channel: {:?}", why);
+
+			return;
+		},
+	};
+	println!("We got it!!");
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use serenity::{Client, async_trait, client::{Context, EventHandler}, framework::{
-		standard::{
-			macros::{command, group},
-			CommandResult,
-		},
-		StandardFramework,
-	}, model::{channel::Message, id::UserId, prelude::Ready}};
-use std::{collections::HashSet, env};
+use chrono::{Date, NaiveDate, Utc};
+use serenity::{Client, async_trait, client::{Context, EventHandler}, framework::{StandardFramework, standard::{CommandResult, macros::{command, group}}}, model::{channel::Message, id::UserId}, prelude::{RwLock, TypeMapKey}};
+
+use std::{collections::{HashMap, HashSet}, env, sync::Arc};
 
 mod conversion;
 mod birthdays;
 
 use birthdays::*;
+
+
+struct TodayDate;
+impl TypeMapKey for TodayDate {
+	type Value = Arc<RwLock<Date<Utc>>>;
+}
 
 #[group]
 #[commands(about,update_db)]
@@ -21,20 +23,40 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
 	async fn message(&self, ctx: Context, msg: Message) {
+		// check if we've sent a birthday reminder today (if a flag_date has already been set to today's date then we have!)
+		println!("{:?}", msg.timestamp);
+		let date = msg.timestamp.date();
+		let flag_date = {
+			let data_read = ctx.data.read().await;
+			let today_date_lock = data_read.get::<TodayDate>().expect("expected a TodayDate").clone();
+			let today_date = today_date_lock.read().await;
+			today_date.clone()
+		};
+		if date != flag_date {
+			let date_lock = {
+				let data_write = ctx.data.read().await;
+				data_write.get::<TodayDate>().expect("Expected a TodayDate").clone()
+			};
+			// set flag date to today's date
+			{
+				let mut flag_date_write = date_lock.write().await;
+				*flag_date_write = date;
+			}
+			notify_users(&ctx,&msg).await;
+		}
 		if !msg.author.bot {
 			if let Some(reply) = conversion::respond_to_msg(&msg.content){
 				msg.reply(&ctx, reply).await.unwrap();
+				println!("{:?}",msg.timestamp.date())
 			}
 		}
 	}
-	async fn ready(&self, _: Context, ready: Ready) {
-        
-    }
 }
 
 #[tokio::main]
 async fn main() {
 	let token: String = env::var("tutorial_bot_token").expect("Token not found");
+	
 	let mut owners = HashSet::new();
 	owners.insert(UserId::from(360433679111159808));
 	let framework = StandardFramework::new()
@@ -46,7 +68,14 @@ async fn main() {
 		.framework(framework)
 		.await
 		.expect("Could not start Discord");
-
+	
+	// creating empty globals
+	{
+		let mut data = client.data.write().await;
+		data.insert::<BirthdaysDb>(Arc::new(RwLock::new(HashMap::default())));
+		data.insert::<TodayDate>(Arc::new(RwLock::new(Date::<Utc>::from_utc(NaiveDate::from_yo(2021, 1), Utc))));
+	}
+	
 	client.start().await.expect("The bot stopped");
 }
 
